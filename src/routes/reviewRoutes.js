501eab44388eb3d0d07fa6d4c2bd2de1fe03cf79
DavidKoljnrekaj <koljnrekajdavid@gmail.com>
2023-12-09 18:42:17 +0100

    review testing

--- src/routes/reviewRoutes.js
+++ src/routes/reviewRoutes.js
@@ -7,4 +7,5 @@ const router = express.Router();
 router.post('', authenticateToken, reviewController.addReview);
 router.get('/:movieId', reviewController.getReviews);
 router.delete('/:movieId', authenticateToken, reviewController.deleteReview);
+
 module.exports = router;
